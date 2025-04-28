a47445c29b09fced2bbe9f5e4b0d5e3315e53332
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-28 12:54:50 -0300

    feat: mark as completed/pending

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -8,13 +8,15 @@ import TodoList from "./todoList";
 const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
-constructor(props) {
+    constructor(props) {
         super(props)
         this.state = { description: '', list: [] }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
 
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
 
         this.refresh()
@@ -33,18 +35,29 @@ constructor(props) {
         axios.post(URL, { description }).then(resp => this.refresh())
     }
 
-    handleRemove(todo){
+    handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true }).then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false }).then(resp => this.refresh())
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm description={this.state.description}
-                handleChange={this.handleChange}
-                handleAdd={this.handleAdd} />
+                <TodoForm
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList list={this.state.list}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
                     handleRemove={this.handleRemove} />
             </div>
         )
