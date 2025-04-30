843ed760d6af41cf8d9e474155cbee646363cb96
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-30 10:22:01 -0300

    feat: action creator changedescription, setting up redux dev tools, search action creator, applying middleware (redux-promise)

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,21 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler livro',
-        list: [{
-            _id: 1,
-            description: 'pagar fatura do cartao',
-            done: true
-        },{
-            _id: 2,
-            description: 'reuniao com a equipe as 10:00',
-            done: false 
-        }, {
-            _id: 3,
-            description: 'consulta medicas na terça depois do almoço',
-        }]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer
